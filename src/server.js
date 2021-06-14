964122ef5d495a6f43ce7e10aa3eaaf5337800ed
cse-mkamble <76428798+cse-mkamble@users.noreply.github.com>
2021-06-14 12:16:31 +0530

    create auth.js files.

--- src/server.js
+++ src/server.js
@@ -1,30 +1,48 @@
 const express = require('express')
+const connectDB = require('./config/db')
 const env = require('dotenv')
 const cors = require('cors')
-const connectDB = require('./config/db')
-const errorHandler = require('./middleware/error')
+const cookieParser = require('cookie-parser')
+const socketServer = require('./socketServer')
+const { ExpressPeerServer } = require('peer')
+const path = require('path')
 
 // environment variable or you can say constants
 const app = express()
 env.config()
 app.use(express.json())
 app.use(cors())
+app.use(cookieParser())
 
 // mongoose Databases connection string
 connectDB()
 
+// Socket
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+io.on('connection', socket => {
+    SocketServer(socket)
+})
+
+// Create peer server
+ExpressPeerServer(http, { path: '/' })
+
 // routes
-// const authRoutes = require('./routes/auth')
-// const privateRoutes = require('./routes/private')
+const authRoutes = require('./routes/auth')
 
 // api
-// app.use('/api/auth', authRoutes)
-// app.use('/api/private', privateRoutes)
+app.use('/api/auth', authRoutes)
 
-// Error Handler (Should be last piece of middleware)
-app.use(errorHandler)
+// 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 // Listing Port
-app.listen(process.env.PORT, () => {
+http.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
 })
